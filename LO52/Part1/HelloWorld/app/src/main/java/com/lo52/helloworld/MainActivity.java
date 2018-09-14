96665e3a541468e659a6740915f59c3ac902c524
koassivi <koassivi.loho@diji.fr>
2018-09-14 12:39:02 +0200

    button click implementation

--- LO52/Part1/HelloWorld/app/src/main/java/com/lo52/helloworld/MainActivity.java
+++ LO52/Part1/HelloWorld/app/src/main/java/com/lo52/helloworld/MainActivity.java
@@ -1,13 +1,26 @@
 package com.lo52.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button_hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_hello = findViewById(R.id.btn_id);
+        button_hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent helloac = new Intent(MainActivity.this, HelloWordActivity.class);
+                startActivity(helloac);
+            }
+        });
     }
 }
