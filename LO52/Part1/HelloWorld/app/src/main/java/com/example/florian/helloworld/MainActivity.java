0be8ef408ab77c7c74ddbc251e0d6f3760d9347a
Florian <florian.rifflart@gmail.com>
2018-09-21 11:27:44 +0200

    ajout fichiers

--- LO52/Part1/HelloWorld/app/src/main/java/com/example/florian/helloworld/MainActivity.java
+++ LO52/Part1/HelloWorld/app/src/main/java/com/example/florian/helloworld/MainActivity.java
@@ -1,22 +1,22 @@
 package com.example.florian.helloworld;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 
-public class HelloWorld extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Button button;
 
     public void init() {
-        button = findViewById(R.id.button2);
+        button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent accueil = new Intent(HelloWorld.this, MainActivity.class);
-                startActivity(accueil);
+                Intent helloWorld = new Intent(MainActivity.this, HelloWorld.class);
+                startActivity(helloWorld);
             }
         });
     }
@@ -24,8 +24,7 @@ public class HelloWorld extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_world);
+        setContentView(R.layout.activity_main);
         init();
     }
-
 }
