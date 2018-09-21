89b51a2bc219e71096d2e1ccc7c53601a982b3a2
ocardoso <olivier.cardoso@utbm.fr>
2018-09-21 12:35:35 +0200

    Bouton fonctionnel

---
+++ LO52/Part1/Hello World/src/main/java/fr/utbm/helloworld/HelloWorldsecondActivity.java
@@ -0,0 +1,13 @@
+package fr.utbm.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorldsecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_worldsecond);
+    }
+}
