ddc20dba866226197ba27eb9f37ed31ada1a9e5d
oacardoso <olivier.al.cardoso@gmail.com>
2018-11-09 11:13:48 +0100

    Config maj droit et modif makefile

--- LO52/Part4/libusb-1.0.3/libusb/Android.mk
+++ LO52/Part4/libusb-1.0.3/libusb/Android.mk
@@ -1,35 +1,17 @@
-# Android build config for libusb
-
 LOCAL_PATH:= $(call my-dir)
-
-LIBUSB_ROOT_REL:= ../..
-LIBUSB_ROOT_ABS:= $(LOCAL_PATH)/../..
-
-# libusb
-
 include $(CLEAR_VARS)
 
-LIBUSB_ROOT_REL:= ../..
-LIBUSB_ROOT_ABS:= $(LOCAL_PATH)/../..
-
-LOCAL_SRC_FILES := \
-  $(LIBUSB_ROOT_REL)/libusb/core.c \
-  $(LIBUSB_ROOT_REL)/libusb/descriptor.c \
-  $(LIBUSB_ROOT_REL)/libusb/io.c \
-  $(LIBUSB_ROOT_REL)/libusb/sync.c \
-  $(LIBUSB_ROOT_REL)/libusb/os/linux_usbfs.c \
-  $(LIBUSB_ROOT_REL)/libusb/os/darwin_usb.c
+LOCAL_SRC_FILES:= \
+ core.c \
+ descriptor.c \
+ io.c \
+ sync.c \
+ os/linux_usbfs.c
 
 LOCAL_C_INCLUDES += \
-  $(LOCAL_PATH)/.. \
-  $(LIBUSB_ROOT_ABS)/libusb \
-  $(LIBUSB_ROOT_ABS)/libusb/os
+external/libusb-1.0.3/ \
+external/libusb-1.0.3/libusb/ \
+external/libusb-1.0.3/libusb/os
 
-LOCAL_EXPORT_C_INCLUDES := \
-  $(LIBUSB_ROOT_ABS)/libusb
-
-LOCAL_LDLIBS := -llog
-
-LOCAL_MODULE := libusb1.0
-
+LOCAL_MODULE:= libusb
 include $(BUILD_SHARED_LIBRARY)
