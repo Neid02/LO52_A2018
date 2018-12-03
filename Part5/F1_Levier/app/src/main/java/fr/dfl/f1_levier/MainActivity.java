bd96499ca49d90e65701db88a2c5d20ed35f6c16
Yannick DJOPGANG <nzeneyannick@yahoo.fr>
2018-12-03 08:20:16 +0100

    creation de l'application F1_Levier

---
+++ Part5/F1_Levier/app/src/main/java/fr/dfl/f1_levier/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.dfl.f1_levier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
